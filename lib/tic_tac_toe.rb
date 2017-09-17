a0a21bb9e1da18408d632d6455c24463f7e89c49
Ryan <ryanwbachman@gmail.com>
2017-09-17 01:24:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,7 +14,7 @@ def play(board)
     current_turn += 1
   end
   if won?(board)
-    puts "Congratualtions, player #{winner(board)}"
+    puts "Congratualtions #{winner(board)!}"
   else
     puts "Cat's game!"
   end
